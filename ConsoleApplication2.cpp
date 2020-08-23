a15bc45bb430ebcb6ee025d83cb07db41ac6bebf
18391028 <68807608+18391028@users.noreply.github.com>
2020-08-24 08:35:14 +1000

    Add files via upload
    It's me mario!

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hi! It\'s a me.\n";
+	cout << "Mario!\n";
+	return 0;
+}
